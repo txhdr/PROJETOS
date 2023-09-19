cd91f7a647fc04fe2befaf3020250d7c90e0e2b7
txhdr <109394306+txhdr@users.noreply.github.com>
2023-09-19 17:26:23 -0300

    update de funcionalidade

--- A06-todolist/script.js
+++ A06-todolist/script.js
@@ -1,9 +1,52 @@
 const button = document.querySelector(".button-add-task")
 const input = document.querySelector(".input-task")
+const listaCompleta = document.querySelector('.list-tasks')
 
-function pegarOValorDoInput(){
-    console.log(input.value)
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa(){
+
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    }
+        )
+
+    input.value = ''
+
+    mostrarTarefas()
+}
+
+function mostrarTarefas(){
+
+    let novaLi = ''
+
+    //['comprar cafe', 'estudar']
+
+    minhaListaDeItens.forEach((tarefa, posicao) => {
+
+        novaLi = novaLi +`
+
+        <li class="task">
+            <img src="img/checked.png" alt="check-na-tarefa">
+            <p>${tarefa}</p>
+            <img src="img/trash.png" alt="lixeira" onclick="deletarItem(${posicao})">
+        </li>
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi
 
 }
 
-button.addEventListener('click',pegarOValorDoInput)
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao,1)
+    
+    mostrarTarefas()
+}
+
+function concluirTarefa(){
+
+}
+
+button.addEventListener('click',adicionarNovaTarefa)
